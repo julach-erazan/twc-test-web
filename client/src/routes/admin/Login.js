a77d31ee8bf292b2edd6b5a3a0683f90eb083693
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2024-03-26 23:04:42 +0530

    login

--- client/src/routes/admin/Login.js
+++ client/src/routes/admin/Login.js
@@ -21,9 +21,63 @@ const Login = () => {
     });
 
   return (
-    <div className="w-[350px] h-[550px] p-[10px] my-[20px]">
-      <div className="w-full h-[50px] bg-[#E2ECFC] text-[#5B6271] flex justify-start items-center px-[10px]">
-        <h1 className="text-[23px] font-semibold">Login</h1>
+    <div className="w-[70%] h-full text-[#fff] flex flex-col justify-center items-center relative z-20">
+      <div className="w-[70%]">
+        <h1 className="text-[50px] font-bold">Hi there,</h1>
+        <h2 className="text-[30px] font-semibold">Welcome to our contacts portal</h2>
+
+        <form
+        onSubmit={handleSubmit}
+        autoComplete="off"
+        action="POST"
+        className="w-full text-[#083F46] flex flex-col items-center mt-[50px]"
+      >
+
+        <input
+          id="email"
+          className={`
+                  w-full h-[40px] rounded-[20px] p-[18px] my-[10px]
+                  ${errors.email && touched.email ? "input-error" : ""}
+                `}
+          type="email"
+          placeholder="Email"
+          value={values.email}
+          onChange={handleChange}
+          onBlur={handleBlur}
+        />
+        {errors.email && touched.email && (
+          <p className="error">{errors.email}</p>
+        )}
+
+        <input
+          id="password"
+          className={`
+                  w-full h-[40px] rounded-[20px] p-[18px] my-[10px]
+                  ${errors.password && touched.password ? "input-error" : ""}
+                `}
+          type="password"
+          placeholder="Password"
+          value={values.password}
+          onChange={handleChange}
+          onBlur={handleBlur}
+        />
+        {errors.password && touched.password && (
+          <p className="error">{errors.password}</p>
+        )}
+
+       <div className="w-full h-[80px] flex text-[#fff] justify-between items-center mt-[50px]">
+       <button
+          className="w-[100px] h-[40px] font-semibold rounded-[20px] border-[2px] border-[#fff] hover:text-[#083F46] hover:bg-[#fff] my-[10px]"
+          type="submit"
+        >
+          <h1>login</h1>
+        </button>
+
+          <h2 className="text-[20px] font-semibold">or</h2>
+
+        <u><h2 className="text-[20px] font-semibold"><a href="">Click here to Register</a></h2></u>
+       </div>
+      </form>
       </div>
     </div>
   );
--- client/src/routes/home/Home.js
+++ client/src/routes/home/Home.js
@@ -4,11 +4,37 @@ import Register from "../admin/Register";
 
 const Home = () => {
   return (
-    <div className="w-screen h-screen flex">
-      <div className="w-[30%]"></div>
-      <div className="w-[70%]">
-        <img src="/Images/bg.jpg" alt="bg-image" />
+    <div 
+      className="
+        home w-screen h-screen bg-[url('/src/Images/bg.jpg')] bg-contain
+        overflow-hidden
+
+        after:content-['']
+        after:w-screen after:h-screen after:bg-[#ffffffbe]
+        after:absolute
+        after:z-10
+        after:top-0
+      "
+    >
+      <div 
+        className="
+          w-[50%] h-full bg-[#083F46] relative z-20 flex justify-center items-center
+
+          after:content-['']
+          after:w-[50%] after:h-[110%] after:bg-[#083F46]
+          after:absolute
+          after:z-10
+          after:top-[-5%]
+          after:left-[70%]
+          after:rounded-[50%]
+        ">
+          <Login/>
       </div>
+ 
+      <div className="
+          w-[50%] h-full bg-[#000] relative z-20 flex justify-center items-center">
+            fddfh
+      </div> 
     </div>
   );
 };
